a1d416e67a421a79c7d219ad8f31434de7bbcc80
Ken Eguchi <no.eggchicken@gmail.com>
2019-08-14 02:47:09 +0900

    Add javascript sample

--- index.js
+++ index.js
@@ -18,11 +18,24 @@
         const textNode = newNode.getElementsByClassName("js-text")[0]
         const codeNode = newNode.getElementsByClassName("js-code")[0]
         const spriteNode = newNode.getElementsByClassName("js-sprite")[0]
-        const sprite = new Sprite(animationName, "character1")
+        const sprite = new Sprite("sprite-character1", animationName)
         spriteNode.appendChild(sprite.node)
         textNode.appendChild(document.createTextNode(title))
         codeNode.appendChild(document.createTextNode(sprite.node.outerHTML))
         animationContainer.appendChild(newNode)
     })
     template.parentNode.removeChild(template)
+
+    const node = document.getElementById("js-target")
+    const sprite1 = new Sprite("sprite-character1", "sprite-animation1-1")
+    const sprite2 = new Sprite("1", "1-2")
+    node.appendChild(sprite1.node)
+    node.appendChild(sprite2.node)
+    setInterval(()=>{
+        if(sprite1.animationName == "sprite-animation1-1"){
+            sprite1.setAnimationName("5-3")
+        } else {
+            sprite1.setAnimationName("1-1")
+        }
+    }, 1000)
 })()
--- sprite.js
+++ sprite.js
@@ -1,14 +1,28 @@
 class Sprite {
     constructor(characterName, animationName) {
-        this.characterName = characterName
-        this.animationName = animationName
         this.node = document.createElement("div")
-        this.node.classList.add("sprite", animationName, characterName)
+        this.node.classList.add("sprite")
+        this.setAnimationName(animationName)
+        this.setCharacterName(characterName)
     }
 
     setAnimationName(animationName) {
+        if(!animationName.startsWith("sprite-animation")) {
+            animationName = "sprite-animation" + animationName
+        }
+
         this.node.classList.remove(this.animationName)
         this.node.classList.add(animationName)
         this.animationName = animationName
+    }
+
+    setCharacterName(characterName) {
+        if(!characterName.startsWith("sprite-character")) {
+            characterName = "sprite-character" + characterName
+        }
+
+        this.node.classList.remove(this.characterName)
+        this.node.classList.add(characterName)
+        this.characterName = characterName
     }
 }
